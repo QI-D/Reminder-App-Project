68d31886706dc99444a1911576a75e393868bacd
Steven <zzt2143@hotmail.com>
2020-11-03 15:50:13 -0800

    initialization

---
+++ database.js
@@ -0,0 +1,11 @@
+let Database = {
+    cindy: {
+        reminders: [{title:"grocery",description:"buy milk",completed:false},
+        {title:"2520",description:"assign",completed:false}]
+    },
+    alex: {
+        reminders: []
+    } 
+}
+
+module.exports = Database;
